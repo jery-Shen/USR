d6b4eb4a3af0fdbc1ed87e0d224a6cf309949214
jeri-shen <631059305@qq.com>
2017-04-13 20:56:10 +0800

    MD5

--- src/usr/work/utils/Md5.java
+++ src/usr/work/utils/Md5.java
@@ -2,21 +2,31 @@ package usr.work.utils;
 
 import java.security.MessageDigest;
 
-import sun.misc.BASE64Encoder;
-
 public class Md5 {
 	
-	
-	public static String encode(String str){
-		String newstr = "";
+
+	public final static String encrypt(String plaintext) {
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 		try {
-			MessageDigest md5 = MessageDigest.getInstance("MD5");
-			BASE64Encoder base64en = new BASE64Encoder();
-			newstr = base64en.encode(md5.digest(str.getBytes("utf-8")));
+			byte[] btInput = plaintext.getBytes();
+			// 获得MD5摘要算法的 MessageDigest 对象
+			MessageDigest mdInst = MessageDigest.getInstance("MD5");
+			// 使用指定的字节更新摘要
+			mdInst.update(btInput);
+			// 获得密文
+			byte[] md = mdInst.digest();
+			// 把密文转换成十六进制的字符串形式
+			int j = md.length;
+			char str[] = new char[j * 2];
+			int k = 0;
+			for (int i = 0; i < j; i++) {
+				byte byte0 = md[i];
+				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+				str[k++] = hexDigits[byte0 & 0xf];
+			}
+			return new String(str);
 		} catch (Exception e) {
-			// TODO: handle exception
+			return null;
 		}
-		
-		return newstr;
 	}
 }
