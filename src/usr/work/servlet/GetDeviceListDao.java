6d3888334bfc406fe46412c0b467015b52349bc7
jeriShen <shenyaojie@greatld.com>
2017-06-20 15:26:22 +0800

    a

---
+++ src/usr/work/servlet/GetDeviceListDao.java
@@ -0,0 +1,48 @@
+package usr.work.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
+
+import usr.work.bean.Device;
+import usr.work.bean.Message;
+import usr.work.dao.DeviceDao;
+
+/**
+ * Servlet implementation class GetDeviceList
+ */
+@WebServlet("/GetDeviceListDao")
+public class GetDeviceListDao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int areaId = 0;
+		try {
+			areaId = Integer.parseInt(request.getParameter("areaId"));
+		} catch (Exception e) {}
+		List<Device> deviceList = null;
+		 
+		DeviceDao deviceDao = new DeviceDao();
+		if(areaId==0){
+			deviceList = deviceDao.getList();
+		}else{
+			deviceList = deviceDao.getList(areaId);
+		}
+		String resStr = JSON.toJSONString(new Message(200,deviceList));
+		response.getWriter().println(resStr);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
